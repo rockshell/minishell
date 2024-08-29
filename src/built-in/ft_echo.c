31423ffd5f414bcadd149d9a799ff8c2901a3bf7
sckptt <kiddovpm@gmail.com>
2024-08-29 18:24:08 +0700

    ft_echo added

--- src/built-in/ft_echo.c
+++ src/built-in/ft_echo.c
@@ -6,8 +6,21 @@
 /*   By: vitakinsfator <vitakinsfator@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 16:13:45 by vitakinsfat       #+#    #+#             */
-/*   Updated: 2024/08/29 16:14:00 by vitakinsfat      ###   ########.fr       */
+/*   Updated: 2024/08/29 18:20:30 by vitakinsfat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void ft_echo(char **arguments)
+{
+	int argc;
+	
+	argc = 0;
+	while (arguments[argc])
+		argc++;
+	if (argc == 2)
+		printf("%s\n", arguments[1]);	
+	else if (argc == 3 && ft_strncmp(arguments[1], "-n", 2) == 0)
+		printf("%s", arguments[2]);
+}
