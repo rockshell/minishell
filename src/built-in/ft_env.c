d45dee2d0a12d54129cf7854b9fd1311109d73a3
sckptt <kiddovpm@gmail.com>
2024-08-29 18:25:46 +0700

    ft_env added

--- src/built-in/ft_env.c
+++ src/built-in/ft_env.c
@@ -6,8 +6,17 @@
 /*   By: vitakinsfator <vitakinsfator@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 16:15:32 by vitakinsfat       #+#    #+#             */
-/*   Updated: 2024/08/29 16:15:35 by vitakinsfat      ###   ########.fr       */
+/*   Updated: 2024/08/29 18:24:33 by vitakinsfat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void ft_env(t_appdata *appdata)
+{
+    int i;
+
+    i = -1;
+    while (appdata->env[++i])
+        printf("%s\n", env[i]);
+}
