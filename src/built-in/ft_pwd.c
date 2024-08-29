5b5d79d039cb19412c934b1f6758bb6ae89f5130
sckptt <kiddovpm@gmail.com>
2024-08-29 18:45:15 +0700

    ft_pwd added

--- src/built-in/ft_pwd.c
+++ src/built-in/ft_pwd.c
@@ -6,8 +6,25 @@
 /*   By: vitakinsfator <vitakinsfator@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 16:14:32 by vitakinsfat       #+#    #+#             */
-/*   Updated: 2024/08/29 16:14:36 by vitakinsfat      ###   ########.fr       */
+/*   Updated: 2024/08/29 18:28:47 by vitakinsfat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void ft_pwd(t_appdata *appdata)
+{
+    char *pwd;
+    int i;
+
+    i = 0;
+    while (appdata->env[i])
+    {
+        if (ft_strncmp(appdata->env[i], "PWD=", 4) == 0)
+            pwd = strdup(appdata->env[i] + 4);
+        else 
+            i++;
+    }
+    printf("%s\n", pwd);
+    free(pwd);
+}
